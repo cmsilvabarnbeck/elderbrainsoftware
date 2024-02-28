99168fded14c04ebd540e830e9990dcc5e9e3f7b
Chris Barnbeck <cmsilvabarnbeck@gmail.com>
2024-02-28 16:40:58 -0500

    Delete Mapping Tested

--- src/main/java/com/elderbrainsoftware/controller/UserController.java
+++ src/main/java/com/elderbrainsoftware/controller/UserController.java
@@ -3,6 +3,7 @@ package com.elderbrainsoftware.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,8 +51,9 @@ public class UserController {
 		return user1;
 	}
 	
-	public String deleteUser() {
-		return("User Deleted Successfully!");
+	@DeleteMapping("/users/{userId}")
+	public String deleteUser(@PathVariable("userId") Integer userId) {
+		return("User " + userId + " Deleted Successfully!");
 	}
 
 }
